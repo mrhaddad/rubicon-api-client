447b52d4f2b7c9b91e5b6a221b284ae556627332
Michael Haddad <michael@ludditetechnology.com>
2014-05-26 17:55:11 -0400

    Use current API

--- lib/rubicon-api-client.rb
+++ lib/rubicon-api-client.rb
@@ -3,7 +3,7 @@ require 'net/http'
 require 'net/http/digest_auth'
 module RubiconApiClient
   class RubiconClient
-    @@host = 'http://api.rubiconproject.com'
+    @@host = 'https://api.rubiconproject.com'
 
 
     def whens
@@ -24,19 +24,10 @@ module RubiconApiClient
 
     def execute(path)
       puts path
-
       uri = URI.parse @@host
-      uri.user = @key
-      uri.password = @secret
-
-      net = Net::HTTP.new uri.host    
+      net = Net::HTTP.new uri.host
       req = Net::HTTP::Get.new path
-
-      res = net.request req
-
-      auth = Net::HTTP::DigestAuth.new.auth_header uri, res['www-authenticate'], 'GET'
-      req = Net::HTTP::Get.new path
-      req.add_field 'Authorization', auth
+      req.basic_auth(@key, @secret)
       req['Accept'] = @response_format
       res = net.request req
       res.read_body
@@ -69,24 +60,18 @@ module RubiconApiClient
       args = parse_date date_range
 
       args['site_id'] = site_ids
-      
+
       path = "/seller/api/ips/v1/reports/zone/performance/#{@id}/#{compose_arguments args}"
       execute(path)
     end
-    
-    def ad_hoc_performance_report(dimensions, measures, currency=nil, *date_range)
-      possible_dims = ['date','ad_size','site','zone','country','keyword','campaign','campaign_relationship','partner','agency']
-      possible_measures = ['paid_impressions','total_impressions','revenue','ecpm','rcpm','fill_rate']
 
+    def ad_hoc_performance_report(columns, currency=nil, *date_range)
       args = parse_date date_range
       args['currency'] = currency
-      args['dimensions'] = Array.new
-      args['measures'] = Array.new
-      
-      dimensions.each { |dim| args['dimensions'] << dim if possible_dims.include? dim } if dimensions.is_a? Array
-      measures.each { |measure| args['measures'] << measure if possible_measures.include? measure } if measures.is_a? Array
+      args['columns'] = columns
+      args['source'] = 'standard'
 
-      path = "/seller/api/ips/v2/reports/performance/#{@id}/#{compose_arguments args}"
+      path = "/sellers/api/reports/v1/#{@id}/#{compose_arguments args}"
       execute(path)
     end
 
